8f723307fce0d6e389ec59d8001d156b7a59e051
AramisAra <martinezcruzaj@gmail.com>
2024-05-15 15:20:07 -0400

    Adding the code error handling

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,3 +3,10 @@ const request = require('request');
 
 const link = process.argv[2];
 request.get(link);
+request(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:', response.statusCode);
+  }
+});
