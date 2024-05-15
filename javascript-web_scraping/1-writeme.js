faf7ffedac214f2ebdc44839311c975765cca179
AramisAra <martinezcruzaj@gmail.com>
2024-05-15 12:56:54 -0400

    Remove unuse variable

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fdPath = process.argv[2];
 const fdWrite = process.argv[3];
 
-fs.writeFile(fdPath, fdWrite, (err, data) => {
+fs.writeFile(fdPath, fdWrite, (err) => {
   if (err) {
     console.log(err);
   }
