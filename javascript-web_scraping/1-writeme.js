1151d61fbf373b5a0cdf79f8bf89817d22c2811c
AramisAra <martinezcruzaj@gmail.com>
2024-05-15 13:16:16 -0400

    Fixing lint

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
+
 const fdPath = process.argv[2];
 const fdWrite = process.argv[3];
 
@@ -7,4 +8,4 @@ fs.writeFile(fdPath, fdWrite, (err) => {
   if (err) {
     console.log(err);
   }
-})
+});
